88d025525ed085ba7b18c4dae6b084cfac27233a
prajw0 <141474516+prajw0@users.noreply.github.com>
2024-02-08 13:08:38 +0000

    pushcode

---
+++ 01_basics/03_conversionoperation.js
@@ -0,0 +1,11 @@
+let num=33;
+console.log(typeof num);
+let num2="33abc"
+console.log(typeof num2);
+let valueIntNumber=Number(num2)
+// console.log(typeof valueIntNumber)
+// console.log(valueIntNumber)
+
+let isLoggedIn=0
+let bolleanIsLoggedIn=Boolean(isLoggedIn)
+console.log( bolleanIsLoggedIn)
