ab2c123436c852136c9b641eb54f74fa5730e843
prajw0 <141474516+prajw0@users.noreply.github.com>
2024-02-08 12:50:45 +0000

    push code

--- 01_basics/text.js
+++ 01_basics/text.js
@@ -1,2 +1,7 @@
 console.log("hello")
 console.log("there")
+let age="prjwal";
+console.log(typeof null);  //typeof null is object
+console.log(typeof undefined);  //typeof undefined is undefined
+
+
