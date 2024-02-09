9ad8d55eff9d200a990f9fe83cc8dbafb19cff69
prajw0 <141474516+prajw0@users.noreply.github.com>
2024-02-09 09:34:04 +0545

    text

--- 01_basics/text.js
+++ 01_basics/text.js
@@ -1,9 +1,9 @@
 
+
 console.log("hello")
 console.log("there")
 let age="prjwal";
 console.log(typeof null);  //typeof null is object
 console.log(typeof undefined);  //typeof undefined is undefined
-
 
 
