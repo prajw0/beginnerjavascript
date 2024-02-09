551d835ee5e5704a315858bb7786fa1c468215a3
prajw0 <141474516+prajw0@users.noreply.github.com>
2024-02-09 04:10:51 +0000

    operation

---
+++ 01_basics/04_operation.js
@@ -0,0 +1,8 @@
+console.log(1 + 2) //=>3
+console.log("1" + 2) //=>12
+console.log(1 + "2")  //=>12
+console.log("1" + 2 + 3);  //=>123
+console.log(1 + 2 + "3");  //=>33
+let gameCounter=100
+++gameCounter
+console.log(gameCounter)  //=>101
